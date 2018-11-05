4e5336aacd0a59297d0e6163bd9f9c452c72bc24
Damian Pozimski <damian.pozimski@softpower.pl>
2018-11-05 20:04:05 +0100

    NSwag config build task

--- source/Presentation/Monuments.Manager/Program.cs
+++ source/Presentation/Monuments.Manager/Program.cs
@@ -18,7 +18,23 @@ namespace Monuments.Manager
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
-    }
+                    new WebHostBuilder()
+                        .UseKestrel()
+                        .UseContentRoot(Directory.GetCurrentDirectory())
+                        .ConfigureAppConfiguration((hostingContext, config) =>
+                        {
+                            var env = hostingContext.HostingEnvironment;
+                            config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                                  .AddJsonFile($"appsettings.Local.json", optional: true, reloadOnChange: true)
+                                  .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                            config.AddEnvironmentVariables();
+                        })
+                        .ConfigureLogging((hostingContext, logging) =>
+                        {
+                            logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                            logging.AddConsole();
+                            logging.AddDebug();
+                        })
+                        .UseStartup<Startup>();
+        }
 }
