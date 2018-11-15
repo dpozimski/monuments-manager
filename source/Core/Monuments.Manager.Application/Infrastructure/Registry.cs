e5e5d22d39f7f4140d1a9a99b07ab95b6e448a45
Damian Pozimski <damian.pozimski@softpower.pl>
2018-11-15 06:23:32 +0100

    TerytDictionaryProvider registry

--- source/Core/Monuments.Manager.Application/Infrastructure/Registry.cs
+++ source/Core/Monuments.Manager.Application/Infrastructure/Registry.cs
@@ -3,14 +3,11 @@ using MediatR.Pipeline;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Monuments.Manager.Application.Dictionary.Factories;
-using Monuments.Manager.Application.Dictionary.Providers;
+using Monuments.Manager.Application.Dictionary.Providers.Teryt;
 using Monuments.Manager.Application.Infrastructure.Models;
 using Monuments.Manager.Application.Infrastructure.Pipelines;
 using Monuments.Manager.Application.Monuments.Commands;
-using System;
-using System.Collections.Generic;
 using System.Reflection;
-using System.Text;
 
 namespace Monuments.Manager.Application.Infrastructure
 {
@@ -26,6 +23,7 @@ namespace Monuments.Manager.Application.Infrastructure
             services.AddScoped(typeof(IPipelineBehavior<,>), typeof(AuthenticationPipelineBehavior<,>));
             services.AddMediatR(typeof(CreateMonumentCommand).GetTypeInfo().Assembly);
 
+            services.AddTerytDictionaryProvider(configuration);
             services.AddScoped<IDictionaryProviderFactory, DictionaryProviderFactory>();
             services.AddScoped(s => s.GetService<IDictionaryProviderFactory>().Create());
 
