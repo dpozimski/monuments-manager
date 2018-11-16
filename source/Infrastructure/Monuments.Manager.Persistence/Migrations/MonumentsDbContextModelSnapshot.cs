17a5e91a20ea306fe54841307a05bfaf30f61149
Damian Pozimski <damian.pozimski@softpower.pl>
2018-11-16 22:40:26 +0100

    .net core sdk 2.1.1

--- source/Infrastructure/Monuments.Manager.Persistence/Migrations/MonumentsDbContextModelSnapshot.cs
+++ source/Infrastructure/Monuments.Manager.Persistence/Migrations/MonumentsDbContextModelSnapshot.cs
@@ -15,7 +15,7 @@ namespace Monuments.Manager.Persistence.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.2.0-preview3-35497")
+                .HasAnnotation("ProductVersion", "2.1.0")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
