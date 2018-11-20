49cceab00b4ec07d30ae796003d6bc42e565ab90
Damian Pozimski <damian.pozimski@softpower.pl>
2018-11-20 19:01:10 +0100

    SystemDateTimeProvider tests

---
+++ source/Tests/Monuments.Manager.Infrastructure.Tests/SystemDateTimeProviderTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Monuments.Manager.Infrastructure.Tests
+{
+    public class SystemDateTimeProviderTests
+    {
+        [Fact]
+        public void GetCurrent_ShouldBeLessThanAssertionValue()
+        {
+            //arrange
+            var systemDateTimeProvider = new SystemDateTimeProvider();
+            //act
+            var result = systemDateTimeProvider.GetCurrent();
+            //assert
+            result.Day.Should().Be(DateTime.UtcNow.Day);
+        }
+    }
+}
