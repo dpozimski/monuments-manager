2428c7fded4e887c28930acb4a57545a0d962344
Damian Pozimski <damian.pozimski@softpower.pl>
2018-11-23 21:50:22 +0100

    Navbar layout

--- source/Presentation/Monuments.Manager/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ source/Presentation/Monuments.Manager/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-nav-menu',
@@ -6,7 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-menu.component.css']
 })
 export class NavMenuComponent {
+  private userService: UserService;
+
   isExpanded = false;
+
+  constructor(userService: UserService) {
+    this.userService = userService;
+  }
+
+  isUserLoggedIn(): Boolean {
+    return this.userService.isLoggedIn;
+  }
 
   collapse() {
     this.isExpanded = false;
