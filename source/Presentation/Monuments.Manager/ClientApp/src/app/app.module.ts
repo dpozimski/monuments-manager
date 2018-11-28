27892c33263bc68cc8076fe062bad7c6de2ca77d
Damian Pozimski <damian.pozimski@softpower.pl>
2018-11-28 20:16:41 +0100

    ToastrService

--- source/Presentation/Monuments.Manager/ClientApp/src/app/app.module.ts
+++ source/Presentation/Monuments.Manager/ClientApp/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { BootstrapModalModule } from 'ng2-bootstrap-modal';
+import { ToastrModule } from 'ngx-toastr';
+import { ApiModule } from './api/api.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -13,7 +16,6 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AuthGuard } from './api/security/auth.guard';
-import { ApiModule } from './api/api.module';
 import { SettingsComponent } from './settings/settings.component';
 import { LogoutDialogComponent } from './logout-dialog/logout-dialog.component';
 import { RecoveryPasswordDialogComponent } from './recovery-password-dialog/recovery-password-dialog.component';
@@ -39,6 +41,8 @@ import { RecoveryPasswordComponent } from './recovery-password/recovery-password
     FormsModule,
     ApiModule,
     BootstrapModalModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
     RouterModule.forRoot([
       { path: '', redirectTo: '/home', pathMatch: 'full', canActivate: [AuthGuard] },
       { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
