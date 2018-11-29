4261f5702efdec0d076916e3d79d814521b04ee8
Damian Pozimski <damian.pozimski@softpower.pl>
2018-11-29 19:34:14 +0100

    create app components

--- source/Presentation/Monuments.Manager/ClientApp/src/app/app.module.ts
+++ source/Presentation/Monuments.Manager/ClientApp/src/app/app.module.ts
@@ -11,8 +11,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AuthGuard } from './api/security/auth.guard';
@@ -20,20 +18,24 @@ import { SettingsComponent } from './settings/settings.component';
 import { LogoutDialogComponent } from './logout-dialog/logout-dialog.component';
 import { RecoveryPasswordDialogComponent } from './recovery-password-dialog/recovery-password-dialog.component';
 import { RecoveryPasswordComponent } from './recovery-password/recovery-password.component';
+import { MonumentsComponent } from './monuments/monuments.component';
+import { DictionariesComponent } from './dictionaries/dictionaries.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     LoginComponent,
     RegisterComponent,
     SettingsComponent,
     LogoutDialogComponent,
     RecoveryPasswordDialogComponent,
-    RecoveryPasswordComponent
+    RecoveryPasswordComponent,
+    MonumentsComponent,
+    DictionariesComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -46,8 +48,9 @@ import { RecoveryPasswordComponent } from './recovery-password/recovery-password
     RouterModule.forRoot([
       { path: '', redirectTo: '/home', pathMatch: 'full', canActivate: [AuthGuard] },
       { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
-      { path: 'counter', component: CounterComponent, canActivate: [AuthGuard] },
-      { path: 'fetch-data', component: FetchDataComponent, canActivate: [AuthGuard] },
+      { path: 'monuments', component: MonumentsComponent, canActivate: [AuthGuard] },
+      { path: 'dictionaries', component: DictionariesComponent, canActivate: [AuthGuard] },
+      { path: 'users', component: UsersComponent, canActivate: [AuthGuard] },
       { path: 'settings', component: SettingsComponent, canActivate: [AuthGuard] },
       { path: 'recovery-password', component: RecoveryPasswordComponent },
       { path: 'login', component: LoginComponent },
