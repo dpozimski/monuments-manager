68ab0a67535a488d2e8fc6db40c676ac36228e7d
Damian Pozimski <damian.pozimski@softpower.pl>
2018-12-15 06:52:20 +0100

    Db always prod

--- source/Presentation/Monuments.Manager/ClientApp/src/app/monuments/monuments-list-detail/monuments-list-detail.component.ts
+++ source/Presentation/Monuments.Manager/ClientApp/src/app/monuments/monuments-list-detail/monuments-list-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MonumentsService } from '../services/monuments.service';
+import { MonumentDto, MonumentsClient } from './../../../app/api/monuments-manager-api';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-monuments-list-detail',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./monuments-list-detail.component.css']
 })
 export class MonumentsListDetailComponent implements OnInit {
+  selectedMonument: MonumentDto;
 
-  constructor() { }
+  constructor(private monumentsService: MonumentsService,
+              private monumentsClient: MonumentsClient,
+              private toastr: ToastrService) { }
 
   ngOnInit() {
+    this.monumentsService.selectedMonumentPreviewChanged
+        .subscribe(s => {
+          this.monumentsClient.get(this.selectedMonument.id)
+              .subscribe(result => this.selectedMonument = result,
+                         _ => this.toastr.error('Cannot retrieve detail of monument ' + s.name));
+        })
   }
-
 }
