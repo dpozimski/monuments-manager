2420d3cb6b47cec886fecd17b87227257f65d5f2
Damian Pozimski <damian.pozimski@softpower.pl>
2018-12-15 16:35:15 +0100

    Parallel

--- source/Presentation/Monuments.Manager/ClientApp/src/app/monuments/monuments-pictures-gallery/monuments-pictures-gallery.component.ts
+++ source/Presentation/Monuments.Manager/ClientApp/src/app/monuments/monuments-pictures-gallery/monuments-pictures-gallery.component.ts
@@ -4,54 +4,62 @@ import { MonumentsService } from '../services/monuments.service';
 import { MonumentDto } from '../../api/monuments-manager-api';
 
 @Component({
-  selector: 'app-monuments-pictures-gallery',
-  templateUrl: './monuments-pictures-gallery.component.html',
-  styleUrls: ['./monuments-pictures-gallery.component.css']
+    selector: 'app-monuments-pictures-gallery',
+    templateUrl: './monuments-pictures-gallery.component.html',
+    styleUrls: ['./monuments-pictures-gallery.component.css']
 })
 export class MonumentsPicturesGalleryComponent implements OnInit {
-  galleryOptions: NgxGalleryOptions[];
-  galleryImages: NgxGalleryImage[];
+    galleryOptions: NgxGalleryOptions[];
+    galleryImages: NgxGalleryImage[];
 
-  constructor(private monumentsService: MonumentsService){
+    constructor(private monumentsService: MonumentsService) {
 
-  }
+    }
 
-  ngOnInit() {
+    ngOnInit() {
+        this.monumentsService.selectedMonumentChanged
+            .subscribe(_ => {
+                this.galleryOptions = this.monumentsService.selectedMonument != null ?
+                    [
+                        {
+                            width: '600px',
+                            height: '400px',
+                            thumbnailsColumns: 3,
+                            imageAnimation: NgxGalleryAnimation.Slide,
+                            previewCloseOnClick: true,
+                            previewCloseOnEsc: true,
+                            previewAnimation: true,
+                            preview: true,
+                            previewZoom: true,
+                            previewRotate: true,
+                            previewDownload: true,
+                            downloadIcon: './../../../assets/no-photo-small.png'
+                        }
+                    ] : 
+                    [
+                        {
+                            thumbnails: false
+                        },
+                        {
+                            breakpoint: 500,
+                            width: "100%",
+                            height: "200px"
+                        }
+                    ];
 
-      this.galleryOptions = [
-          {
-              width: '600px',
-              height: '400px',
-              thumbnailsColumns: 4,
-              imageAnimation: NgxGalleryAnimation.Slide,
-              previewCloseOnClick: true,
-              previewCloseOnEsc: true
-          },
-          {
-              breakpoint: 800,
-              width: '100%',
-              height: '600px',
-              imagePercent: 80,
-              thumbnailsPercent: 20,
-              thumbnailsMargin: 20,
-              thumbnailMargin: 20
-          },
-          {
-              breakpoint: 400,
-              preview: true,
-          }
-      ];
-
-      this.monumentsService.selectedMonumentChanged
-          .subscribe(_ => {
-            this.galleryImages = this.monumentsService.selectedMonument.pictures.map(s => {
-                var ngxGalleryImage = new NgxGalleryImage({
-                    small: "data:image/png;base64," + s,
-                    medium: "data:image/png;base64," + s,
-                    big: "data:image/png;base64," + s,
-                });
-                return ngxGalleryImage;
+                this.galleryImages = this.monumentsService.selectedMonument != null ?
+                    this.monumentsService.selectedMonument.pictures.map(s => {
+                        var ngxGalleryImage = new NgxGalleryImage({
+                            small: "data:image/png;base64," + s,
+                            medium: "data:image/png;base64," + s,
+                            big: "data:image/png;base64," + s,
+                        });
+                        return ngxGalleryImage;
+                    }) : [{
+                        small: './../../../assets/no-photo-small.png',
+                        medium: './../../../assets/no-photo-medium.png',
+                        big: './../../../assets/no-photo-big.png'
+                    }];
             });
-          });
-  }
+    }
 }
