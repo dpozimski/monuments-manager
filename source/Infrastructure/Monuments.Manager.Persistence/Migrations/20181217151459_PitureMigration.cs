990b01a4cb17474a8961a3cd7eab9364c0e3a930
Damian Pozimski <damian.pozimski@softpower.pl>
2018-12-17 16:16:22 +0100

    Migration

---
+++ source/Infrastructure/Monuments.Manager.Persistence/Migrations/20181217151459_PitureMigration.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Monuments.Manager.Persistence.Migrations
+{
+    public partial class PitureMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Data",
+                table: "Pictures");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Medium",
+                table: "Pictures",
+                maxLength: 2147483647,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Original",
+                table: "Pictures",
+                maxLength: 2147483647,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Small",
+                table: "Pictures",
+                maxLength: 2147483647,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Medium",
+                table: "Pictures");
+
+            migrationBuilder.DropColumn(
+                name: "Original",
+                table: "Pictures");
+
+            migrationBuilder.DropColumn(
+                name: "Small",
+                table: "Pictures");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Data",
+                table: "Pictures",
+                maxLength: 2147483647,
+                nullable: false,
+                defaultValue: new byte[] {  });
+        }
+    }
+}
