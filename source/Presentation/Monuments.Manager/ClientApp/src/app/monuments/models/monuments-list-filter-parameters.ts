381d4cd898f4fce852de92aa8cd7279d36a1e70a
Damian Pozimski <damian.pozimski@softpower.pl>
2018-12-20 18:50:39 +0100

    6

--- source/Presentation/Monuments.Manager/ClientApp/src/app/monuments/models/monuments-list-filter-parameters.ts
+++ source/Presentation/Monuments.Manager/ClientApp/src/app/monuments/models/monuments-list-filter-parameters.ts
@@ -1,6 +1,6 @@
 export class MonumentsListFilterParameters {
     descSortOrder: boolean = false;
     pageNumber: number = 0;
-    pageSize: number = 5;
+    pageSize: number = 6;
     filter: string;
 }
